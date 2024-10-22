5d6fd5c5f7b9fc5546210317d3d21c73dfae6a03
jpb06 <jpb.06@outlook.com>
2024-10-22 20:50:48 +0200

    fix: upload sourcemaps

--- src/build/upload-sourcemaps.ts
+++ src/build/upload-sourcemaps.ts
@@ -1,19 +1,20 @@
 import { Effect, pipe } from 'effect';
+import { CloudflareR2StorageLayerLive } from 'effect-cloudflare-r2-layer';
 import { runPromise } from 'effect-errors';
 
 import { getBranch } from './dependencies/github';
+import { ensureBucket, uploadServerIndex } from './dependencies/r2';
 
 const task = pipe(
   Effect.gen(function* () {
-    console.info('YOLO', process.env.YOLO);
-    // yield* ensureBucket;
+    yield* ensureBucket;
 
     const branch = yield* getBranch;
-    // yield* uploadServerIndex(branch);
+    yield* uploadServerIndex(branch);
 
     console.info(`✅ mapfile uploaded for branch '${branch}'`);
   }),
-  // Effect.provide(CloudflareR2StorageLayerLive),
+  Effect.provide(CloudflareR2StorageLayerLive),
   Effect.withSpan('upload-sourcemaps'),
 );
 
