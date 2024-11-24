d5fe6cd974ecc8af2fc5a026c93c70651c377580
jpb06 <jpb.06@outlook.com>
2024-11-24 16:30:04 +0100

    fix: parallel

--- src/examples/unknown-error.example.ts
+++ src/examples/unknown-error.example.ts
@@ -1,14 +1,14 @@
 import { readFile as nodeReadFile } from 'node:fs/promises';
 import { Effect, pipe } from 'effect';
 
-const readUser = pipe(
+const readFile = pipe(
   Effect.tryPromise(
     async () => await nodeReadFile('cool.ts', { encoding: 'utf-8' }),
   ),
-  Effect.withSpan('read-user'),
+  Effect.withSpan('read-file'),
 );
 
 export const unknownErrorTask = pipe(
-  Effect.all([Effect.sleep('55 millis'), readUser]),
+  Effect.all([Effect.sleep('55 millis'), readFile]),
   Effect.withSpan('unknown-error-task'),
 );
